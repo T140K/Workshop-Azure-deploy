c128b59f400933c254f8e981b1b2b708ee01389e
Darek <darek1430143@gmail.com>
2023-11-24 18:03:52 +0100

    init

---
+++ AzureDeployDemo/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using AzureDeployDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureDeployDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Greeting = _configuration["MyGreeting"];
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
